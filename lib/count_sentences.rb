ddd8fad6ea90ea62b1962e53ea7c6e182d1f794e
narduc <radnielpadron@gmail.com>
2020-03-15 21:17:18 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,30 @@ require 'pry'
 
 class String
 
+  # "HI!".end_with?("!")
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    arr = []
+    arr = self.split
+    i = 0
+    total = 0
+    while i < arr.length
+      if arr[i].sentence? || arr[i].question? || arr[i].exclamation? 
+        total += 1
+      end
+      i += 1
+    end
+    return total
   end
 end
